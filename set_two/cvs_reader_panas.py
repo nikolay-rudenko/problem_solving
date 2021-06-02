a0a3f7eac392f000358bd7182580818f3fd61127
Karma Dorge <37703436+karmadorge@users.noreply.github.com>
2021-06-02 20:34:25 +0300

    additional changes

--- set_two/cvs_reader_panas.py
+++ set_two/cvs_reader_panas.py
@@ -5,11 +5,11 @@ from sqlalchemy import create_engine
 # This CSV doesn't have a header so pass
 # column names as an argument
 columns = [
-  "sepal_length",
-  "sepal_width",
-  "petal_length",
-  "petal_width",
-  "class"
+  'book_title',
+  'author',
+  'publisher',
+  'pub_date',
+  'isbn'
 ]
 
 # Instantiate sqlachemy.create_engine object
@@ -17,10 +17,10 @@ engine = create_engine('postgresql://nikol:moonbeam@localhost:5432/test')
 
 # Create an iterable that will read "chunksize=1000" rows
 # at a time from the CSV file
-for df in pd.read_csv("data/books.csv",names=columns,chunksize=1000):
-  df.to_sql(
-    'books',
-    engine,
-    index=False,
-    if_exists='append' # if the table already exists, append this data
-  )
+for df in pd.read_csv("data/books.csv", names=columns, chunksize=1000, header=None):
+    df.to_sql(
+        'books',
+        engine,
+        index=False,
+        if_exists='append'  # if the table already exists, append this data
+    )
